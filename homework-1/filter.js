65f1e2f92d77cb5567bb3c864d2b14cdbc9148d6
galtnie <hereby@protonmail.com>
2018-07-31 22:12:45 +0300

    fixed files

--- homework-1/filter.js
+++ homework-1/filter.js
@@ -1,12 +1,16 @@
 let arr = [123, 234, 345, 456, 567, 678, 789]
 let notArr = ''
 
-let filter = function (array) {                   //filtering odd numbers
+let callback = function(element){           
+    return element % 2 === 1;           //filtering odd numberss
+}
+
+let filter = function (array, callback) {                   
     if (Array.isArray(array)) {
         let anotherArray = []
         counter = 0
         for (let i = 0; i < array.length; i++) {
-            if (array[i] % 2 === 0) {
+            if (callback(array[i])) {
                 anotherArray[counter] = array[i]
                 counter++
             }
@@ -17,5 +21,5 @@ let filter = function (array) {                   //filtering odd numbers
     }
 }
 
-console.log(filter(arr))
+console.log(filter(arr, callback))
 //console.log(filter(notArr))
